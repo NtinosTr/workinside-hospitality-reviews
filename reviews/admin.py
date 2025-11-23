6a77692b60c14536f19ba241c57f7866cbf59c76
NtinosTr <kostastrofin@gmail.com>
2025-11-23 14:04:50 +0100

    Add Django admin panel for Hotels, Departments, Reviews

--- reviews/admin.py
+++ reviews/admin.py
@@ -5,15 +5,18 @@ from .models import Hotel, Department, Review
 @admin.register(Hotel)
 class HotelAdmin(admin.ModelAdmin):
     list_display = ("name", "location")
+    search_fields = ("name", "location")
 
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
     list_display = ("name",)
+    search_fields = ("name",)
 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ("hotel", "department", "rating", "email", "created_at")
-    list_filter = ("hotel", "department", "rating")
-    search_fields = ("hotel__name", "department__name", "comment", "email")
+    list_display = ("hotel", "department", "rating", "email", "created_at", "is_verified", "is_sensitive")
+    list_filter = ("hotel", "department", "rating", "is_verified", "is_sensitive")
+    search_fields = ("hotel__name", "department__name", "comment", "email", "name")
+    ordering = ("-created_at",)
